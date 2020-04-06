4b75e7c9ea4c79a3d2c35fc3abc5be07b5cfc597
Lauren St. Clair <lauren.d.stclair@gmail.com>
2020-04-06 11:15:40 -0700

    Add config.php TODO

--- config.php
+++ config.php
@@ -5,6 +5,8 @@ define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'demo');
+
+// TODO: Confirm database server, username, password
  
 /* Connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
