da218181d1ac4047dc751ac4278aabd76d9b5c13
Lauren St. Clair <lauren.d.stclair@gmail.com>
2020-04-06 11:12:10 -0700

    Add config.php TODO

--- config.php
+++ config.php
@@ -4,8 +4,10 @@ Replace with server credentials */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'demo');
+define('DB_NAME', 'rebbit');
  
+// TODO: Define if we need a username and password for database
+
 /* Connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
