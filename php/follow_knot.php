ffc1f27938824e33f222b110b4b0e7bc88116d74
kzlecha <kzlecha@gmail.com>
2020-04-08 13:15:33 -0700

    ADD follow knot functionality

--- php/follow_knot.php
+++ php/follow_knot.php
@@ -2,6 +2,8 @@
 
     session_start();
 
+    require_once "include/config.php";
+
     $loggedin = isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true;
 
     // if the user is not logged in, exit
@@ -11,5 +13,32 @@
 
     $user_id = $_SESSION["user_id"];
     $knot_id = $_GET["knot_id"];
+
+    $sql = "INSERT INTO FollowingKnot (user_id, knot_id, create_date) VALUES (?, ?, NOW())";
+
+    if($stmt = mysqli_prepare($link, $sql)){
+        // Bind variables to the prepared statement as parameters
+        mysqli_stmt_bind_param($stmt, "ii", $param_userid, $param_knotid);
+        
+        // Set parameters
+        $param_userid = $user_name;
+        $param_knotid = $knot_id;
+        
+        // Attempt to execute the prepared statement
+        if(mysqli_stmt_execute($stmt)){
+            // Redirect to login page
+            header("location: knot.php?knot_id=".$knot_id."");
+        } else{
+            echo "Something went wrong. Please try again later.";
+        }
+
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+
+    // Close connection
+    mysqli_close($link);
+
+
 
 ?>
